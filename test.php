323b7078d96fa42862362469594cb255e79a48fb
Teracotta <blue_red@op.pl>
2015-02-25 22:28:35 +0000

    added a sample test case to see if php/xampp was running correctly

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+
+require 'main.php';
+
+class HelloTest extends PHPUnit_Framework_TestCase
+{
+	public function testHello()
+	{
+		$main = New Main;
+		$response = $main->respond("Hi");
+		$this->assertEquals("Hello", $response);
+	}
+}
