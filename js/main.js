ea39a1b8589688f151006aa31f5eb3d5a9756861
Teracotta <blue_red@op.pl>
2015-02-28 12:42:30 +0000

    completely fixed scoreboard scores saved correctly added dependency on last non-strike and non-spare scores (not taken into consideration for frameNumber 11+) restart button enabled according to the game status
    restart option properly resets all components

--- js/main.js
+++ js/main.js
@@ -12,6 +12,7 @@ var startPanel;
 var players_printed;
 var information;
 var comment;
+var restartButton;
 
 var gamePanel;
 var frameNumber;
@@ -20,6 +21,7 @@ var scoreField;
 var scoreButton;
 var scoreBoard;
 var maxPins;
+var noExtraRounds;
 
 function writeName() {
 	namePanel = document.getElementById('namePanel');
@@ -144,10 +146,13 @@ function gameOn() {
 	frameNumber = document.getElementById('frameNumber');
 	ballNumber = document.getElementById('ballNumber');
 	scoreField = document.getElementById('scoreField');
+	scoreButton = document.getElementById('scoreButton');
+	restartButton = document.getElementById('restartButton');
 
 	frameNumber.innerHTML = frameCounter;
 	ballNumber.innerHTML = ballCounter;
 	gamePanel.hidden = false;
+	restartButton.disabled = false;
 	scoreField.focus();
 	maxPins = 10;
 
@@ -157,6 +162,8 @@ function gameOn() {
 function submitScore() {
 	scoreButton = document.getElementById('scoreButton');
 	comment = document.getElementById('comment');
+	noExtraRounds = true;
+
 
 	if(frameCounter < 11) {	
 		if(!verifySubmittedScore(scoreField.value)) 
@@ -197,8 +204,9 @@ function submitScore() {
 				}
 			}
 			game[playerCounter]["totalScore"] += parseInt(scoreField.value);
-			game[playerCounter][frameCounter][ballCounter] = parseInt(scoreField.value);
-			
+			if(!(game[playerCounter][frameCounter]["spare"] || game[playerCounter][frameCounter]["strike"]))
+				game[playerCounter][frameCounter][ballCounter] = parseInt(scoreField.value);
+
 			ballCounter = 1;
 			playerCounter++;							// next player
 			maxPins = 10;
@@ -207,25 +215,31 @@ function submitScore() {
 				frameCounter++;
 			}
 		}
-		console.log("woo");
 		updateNews();
 		frameNumber.innerHTML = frameCounter;
 		ballNumber.innerHTML = ballCounter;
-		console.log(game);
-
+		console.log(game);					
+		console.log(noExtraRounds);
 	}
 	// extra points possible
 	else {
+
+		//else {
 		if((game[playerCounter][10]["spare"] || game[playerCounter][10]["strike"]) && frameCounter != 13) {
-			if (parseInt(scoreField.value) < 0 || scoreField.value % 1 != 0) {
+			if (scoreField.value == "" || parseInt(scoreField.value) < 0 || scoreField.value % 1 != 0) {
 				information.innerHTML = "Please input only integer values in the score indicating how many pins you have knocked over (max: 10).";
 				return;
 			}
+			if(scoreField.value == 10)
+				comment.innerHTML = "EXTRA STRIKE!";
+			else
+				comment.innerHTML = "...";
 			// last spare - 1 extra round
 			if(game[playerCounter][10]["spare"]) {
 				game[playerCounter]["totalScore"] += parseInt(scoreField.value);
 				game[playerCounter][frameCounter] = parseInt(scoreField.value);
 				frameCounter++;
+				noExtraRounds = true;
 			}
 			// last strike - 2 extra rounds
 			else {
@@ -235,6 +249,7 @@ function submitScore() {
 				else 
 				 	game[playerCounter]["totalScore"] += parseInt(scoreField.value);
 				game[playerCounter][frameCounter] = parseInt(scoreField.value);
+				noExtraRounds = true;
 			}
 			frameCounter++;
 		}
@@ -243,16 +258,44 @@ function submitScore() {
 			playerCounter++;
 		}
 		updateNews();
-	}		
+	}
+
+		if((!(game[playerCounter][10]["spare"]) && !(game[playerCounter][10]["strike"])) && frameCounter == 11 ) {
+
+	}
+
+
+	if(frameCounter == 11) {		// no final strike or spare
+		checkIfPlayerHasExtraRounds();
+		for (var i = 0; i < game.length; i++) {
+			if(game[i][10]["strike"] || game[i][10]["spare"]) {
+				noExtraRounds = false;
+				break;
+			}
+		}
+		if(noExtraRounds) {
+			endGame();
+			return;
+		}
+	}
 
 	console.log(game);
-	if(frameCounter == 13 && playerCounter+1 == playerNames.length)	{
-			frameNumber.innerHTML = "-- GAME COMPLETE --";
-			ballNumber.innerHTML = "-- GAME COMPLETE --";
-			endGame();
-		}
+	if((frameCounter == 13 && playerCounter+1 == playerNames.length && noExtraRounds) || (playerCounter >= playerNames.length)	){
+		endGame();
+	}
 	scoreField.value = "";
 	scoreField.focus();
+}
+
+function checkIfPlayerHasExtraRounds() {
+	if(!game[playerCounter][10]["spare"] && !game[playerCounter][10]["strike"]) {
+		console.log("DUH");
+		playerCounter++;
+		if(playerCounter == game.length)
+			endGame();
+		updateNews();
+		checkIfPlayerHasExtraRounds();
+	}
 }
 
 function updateNews() {
@@ -281,13 +324,6 @@ function verifySubmittedScore(score) {
 	return true;
 }
 
-function endGame() {
-	information.innerHTML = "All the scores are now recorded! And the winner is...";
-	scoreField.disabled = true;
-	scoreButton.disabled = true;
-	return;
-}
-
 function updateBoard() {
 	scoreBoard = document.getElementById('scoreBoard');
     var text = "<tr><th>Name</th><th colspan=\"2\">1</th><th colspan=\"2\">2</th><th colspan=\"2\">3</th><th colspan=\"2\">4</th><th colspan=\"2\">5</th><th colspan=\"2\">6</th><th colspan=\"2\">7</th><th colspan=\"2\">8</th><th colspan=\"2\">9</th><th colspan=\"2\">10</th><th>11 (extra)</th><th>12 (extra)</th><th>Score</th></tr>";
@@ -302,8 +338,7 @@ function updateBoard() {
 }
 
 function restart() {
-	var r = confirm("You are about to completely restart the game. Continue?");
-	if (r == true) {
+	if (confirm("You are about to completely restart the game. Continue?")) {
 	   	playerNames = new Array();
 		game = new Array();
 		updateBoard();
@@ -311,11 +346,55 @@ function restart() {
 		nameField.hidden = false;
 		startPanel.hidden = false;
 		startButton.disabled = true;
+		scoreField.disabled = false;
+		restartButton.disabled = true;
+		scoreButton.disabled = false;
 		gamePanel.hidden = true;
 		playerCounter = 0;
 		frameCounter = 1;
 		ballCounter = 1;
 		players_printed.innerHTML = "None";
-		information.innerHTML = "The game has been restarted. Welcome to the Virtual Bowling Scoring System!"
+		information.innerHTML = "The game has been restarted. Welcome to the Virtual Bowling Scoring System!";
+		nameField.focus();
 	}
+}
+
+function endGame() {
+	var topScores = new Array();
+	for (var i = 0; i < game.length; i++) topScores[i] = 0;
+	var topPlayers = new Array();
+	for (var i = 0; i < game.length; i++) topPlayers[i] = "";
+
+	frameNumber.innerHTML = "-- GAME COMPLETE --";
+	ballNumber.innerHTML = "-- GAME COMPLETE --";
+
+	for (var i = 0; i < game.length; i++) {
+		if(game[i].totalScore > topScores[0]) {
+			topScores = [];
+			topScores[0] = game[i].totalScore;
+			topPlayers = [];
+			topPlayers[0] = game[i].name;
+		}
+		if(game[i].totalScore == topScores[0]) {
+			topScores[topScores.length] = game[i].totalScore;
+			topPlayers[topPlayers.length] = game[i].name;
+		}
+	}
+
+	if(topScores.length == 1)
+		information.innerHTML = "All the scores are now recorded! And the winner is...";
+	else if(topScores.length = game.length)
+		information.innerHTML = "All the scores are now recorded! Everyone has the top score, everyone is a winner!";
+	else {
+		information.innerHTML = "All the scores are now recorded! There is more than one winner! They are: ";
+		for(var i = 0; i < topScores.length; i++)
+			information.innerHTML += topPlayers[i] + " (score " + topScores[i] + "),";
+		// information.innerHTML = information.innerHTML.substring(0, str.length - 1);
+		 information.innerHTML = information.innerHTML.value.replace(/,$/, "") + ".";
+	}
+	scoreField.disabled = true;
+	scoreButton.disabled = true;
+
+
+	return;
 }
